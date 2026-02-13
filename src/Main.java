6033e618880aa2afb954c01098e3a949698a6bf2
LTL430 <lifengwu430@gmail.com>
2026-02-12 20:42:25 -0500

    v1.0

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		int b;
+		int c;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b=sc.nextInt();
+		c=a+b;
+		System.out.print(c);
+	}
+
+}
